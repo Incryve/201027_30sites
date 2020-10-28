ccfe8ba9217fc7edff879e0c5b3395e94132e298
Yve <yve.salazar@gmail.com>
2020-10-28 13:10:57 -0300

    Feature Changing color function

--- projects/90sites/site01_colorcicles/main.js
+++ projects/90sites/site01_colorcicles/main.js
@@ -3,13 +3,26 @@ var btnElement = document.querySelector('#btn');
 var containerElement = document.querySelector('#circle_container');
 
 btnElement.onclick = function newCircle() {
-  var divElement = document.createElement('div');
-  divElement.setAttribute('class', 'circle');
+  var circleElement = document.createElement('div');
+  circleElement.setAttribute('class', 'circle');
 
-  divElement.style.width = "100px";
-  divElement.style.height = "100px";
-  divElement.style.borderRadius = "50px";
-  divElement.style.backgroundColor = '#900C3F';
+  circleElement.style.width = "100px";
+  circleElement.style.height = "100px";
+  circleElement.style.borderRadius = "50px";
+  circleElement.style.backgroundColor = '#900C3F';
   
-  containerElement.appendChild(divElement);
+  circleElement.onmouseover = function() {
+    circleElement.style.backgroundColor = getRandomColor();
+  }
+  
+  containerElement.appendChild(circleElement);
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
