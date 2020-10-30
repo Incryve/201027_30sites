c9518ec7d3edb923ce31189a9bb43e03e3c85682
Yve <yve.salazar@gmail.com>
2020-10-30 19:35:14 -0300

    S02 Feature main js

---
+++ projects/90sites/site02_rockpaperscissorslizardspock/main.js
@@ -0,0 +1,88 @@
+var rock = document.querySelector('.rock');
+var paper = document.querySelector('.paper');
+var scissors = document.querySelector('.scissors');
+var lizard = document.querySelector('.lizard');
+var spock = document.querySelector('.spock');
+
+var hands = {rock: rock, paper: paper, scissors: scissors, lizard: lizard, spock: spock};
+var options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+var playerDecision;
+var compDecision;
+console.log(typeof compDecision);
+console.log(typeof playerDecision);
+
+var btnElement = document.querySelectorAll('.btn');
+console.log(typeof btnElement);
+
+btnElement.forEach(function(button) {
+  button.onclick = function start() {
+    playerDecision = this.id;
+    
+    compDecision = options[Math.floor(Math.random()*5)];
+
+    // document.querySelector('.option').style.display = 'none';
+    document.querySelectorAll('.option').forEach((option) => option.style.display = 'none');
+    
+    countdown();
+  }
+});
+
+function countdown() {
+  var i = 3;
+
+  count();
+
+  function count() {
+    document.querySelector('.score').innerHTML = i;
+
+    if (i == 0) {
+      displayResults();
+    } else {
+      i -= 1;
+      setTimeout(count, 500);
+    }
+  }
+};
+
+function displayResults() {
+  document.querySelector('.score').innerHTML = 'SHOOT';
+  // document.querySelector('.computer_left .option').id = compDecision;
+  // document.querySelector('.player_right .option').id = playerDecision;
+
+  document.querySelector(`.player_right .${playerDecision}`).style.display = 'block';
+  document.querySelector(`.computer_left .${compDecision}`).style.display = 'block';
+  
+  console.log(compDecision);
+  console.log(playerDecision);
+
+  setTimeout(function() {
+    if (playerDecision === compDecision) {
+      document.querySelector('.score').innerHTML = 'TIE';
+    } else if (playerDecision === 'rock' && compDecision === 'scissors') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'rock' && compDecision === 'lizard') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'paper' && compDecision === 'rock') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'paper' && compDecision === 'spock') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'scissors' && compDecision === 'paper') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'scissors' && compDecision === 'lizard') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'lizard' && compDecision === 'paper') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'lizard' && compDecision === 'spock') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'spock' && compDecision === 'rock') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else if (playerDecision === 'spock' && compDecision === 'scissors') {
+      document.querySelector('.score').innerHTML = 'WIN';
+    } else {
+      document.querySelector('.score').innerHTML = 'LOSE';
+    }
+
+    // btnElement.onclick = start();
+  }, 400);
+};
